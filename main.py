89b0ae708286e7dd3a1124a432674483e32268c2
Zaaees <rhiumroleplay@gmail.com>
2025-01-12 19:41:13 +0100

    Espace

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ class CustomBot(commands.Bot):
         #await self.load_extension('cogs.souselement')
         await self.load_extension('cogs.ticket')
         await self.load_extension('cogs.validation')
+        await self.load_extension('cogs.Espace')
 
         # Synchroniser les commandes
         await self.tree.sync()
